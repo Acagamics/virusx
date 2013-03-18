0780f07a5d86cf2ace24c17b9e54762dedb0a50e
gebert <gebertenrico@googlemail.com>
2013-03-18 18:15:44 +0100

    item stuff

--- ParticleStormControl/ParticleStormControl/InGame/Player.cs
+++ ParticleStormControl/ParticleStormControl/InGame/Player.cs
@@ -537,6 +537,7 @@ namespace ParticleStormControl
                         (item as DangerZone).Position = cursorPosition;
                         (item as DangerZone).Activate();
                     }
+                    item = null;
                 }
             }
             
