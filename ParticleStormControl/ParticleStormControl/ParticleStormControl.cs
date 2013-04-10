58a7653bdaa5862aeb1ac4f7d41bd1af7592c023
Andreas Reich (@Home) <andreas@acagamics.de>
2013-04-10 18:31:07 +0200

    fixed menu background

--- ParticleStormControl/ParticleStormControl/ParticleStormControl.cs
+++ ParticleStormControl/ParticleStormControl/ParticleStormControl.cs
@@ -19,6 +19,7 @@ using System.Threading.Tasks;
 using ParticleStormControl;
 using ParticleStormControl.Menu;
 using System.Collections.Generic;
+using System.Linq;
 
 #endif
 
@@ -157,8 +158,9 @@ namespace ParticleStormControl
         {
             // generate cell positions for menu background
             Vector2 relativeMax = new Vector2(GraphicsDevice.Viewport.AspectRatio, 1.0f);
-            List<Vector2> cellPositions = Level.GenerateCellPositions(7, 4, 0.09f, new Vector2(-0.1f), relativeMax);
+            List<Vector2> cellPositions = Level.GenerateCellPositions(7, 4, 0.09f, new Vector2(0.0f), relativeMax);
             background.Resize(GraphicsDevice, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), cellPositions, relativeMax);
+            background.UpdateColors(Enumerable.Repeat(Color.DimGray, cellPositions.Count).ToArray()); 
         }
         
         /// <summary>
