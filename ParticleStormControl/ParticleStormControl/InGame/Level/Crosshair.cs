077d9793cf73872dbb0f21f6d2ee3914624d1d5c
Andreas Reich (@Laptop) <andreas@acagamics.de>
2013-04-11 10:44:41 +0200

    * items overwritable with new items gathered * better crosshair (?)

--- ParticleStormControl/ParticleStormControl/InGame/Level/Crosshair.cs
+++ ParticleStormControl/ParticleStormControl/InGame/Level/Crosshair.cs
@@ -16,12 +16,14 @@ namespace ParticleStormControl
         public Vector2 ParticleAttractionPosition { get; set; }
 
         private Texture2D crossHairTexture;
+     //   private Texture2D glowTexture;
 
         public Crosshair (int playerindex, ContentManager contentManager) :
             base(Vector2.One, 0.05f)   // updated every frame, so why care..
         {
             PlayerIndex = playerindex;
-            this.crossHairTexture = contentManager.Load<Texture2D>("basic_crosshair");;
+            this.crossHairTexture = contentManager.Load<Texture2D>("basic_crosshair");
+   //         this.glowTexture = contentManager.Load<Texture2D>("glow");
         }
 
         public override void SwitchPlayer(int[] playerSwitchedTo)
@@ -32,6 +34,10 @@ namespace ParticleStormControl
 
         public override void Draw_AlphaBlended(SpriteBatch spriteBatch, Level level, float totalTimeSeconds)
         {
+            // black glow
+ //           spriteBatch.Draw(glowTexture, level.ComputePixelRect_Centered(ParticleAttractionPosition, Size * 1.5f), null, new Color(0.5f, 0.5f, 0.5f, 0.5f));
+
+            // background
             Color color = Settings.Instance.GetPlayerColor(PlayerIndex);
             spriteBatch.Draw(crossHairTexture, level.ComputePixelRect(ParticleAttractionPosition, Size), null, color,
                                 -totalTimeSeconds, new Vector2(crossHairTexture.Width * 0.5f, crossHairTexture.Height * 0.5f),
