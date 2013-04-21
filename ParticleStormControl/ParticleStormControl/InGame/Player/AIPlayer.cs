6124545674484ac6adca92857b50147ef6244caa
gebert <gebertenrico@googlemail.com>
2013-04-21 18:01:31 +0200

    ai: some minor improvments with target selection; ai now uses collected items ^^

--- ParticleStormControl/ParticleStormControl/InGame/Player/AIPlayer.cs
+++ ParticleStormControl/ParticleStormControl/InGame/Player/AIPlayer.cs
@@ -13,23 +13,95 @@ namespace ParticleStormControl
 
         #region control variables
 
-        Vector2 targetPosition = Vector2.Zero;
+        private Vector2 targetPosition = Vector2.Zero;
+        private SpawnPoint targetSpawnPoint = null;
+        //private Item targetTarget = null;
+
+        private float maxTargetTime = 5f;
+        private float currentTargetTime = 2.5f;
+        private bool TargetTimeReached { get { return currentTargetTime >= maxTargetTime; } }
 
         #endregion
 
         public AIPlayer(int playerIndex, int virusIndex, int colorIndex, GraphicsDevice device, ContentManager content, Texture2D noiseTexture) :
             base(playerIndex, virusIndex, colorIndex, device, content, noiseTexture)
         {
+            targetPosition = particleAttractionPosition = cursorPosition = cursorStartPositions[playerIndex];
         }
 
         public override void UserControl(float frameTimeInterval, Level level)
         {
-            SelectTarget(level);
+            CheckItems(level);
+            CheckTargets();
+            if (TargetTimeReached)
+            {
+                SelectTarget(level);
+                currentTargetTime = 0.0f;
+            }
+            else
+            {
+                currentTargetTime += frameTimeInterval;
+            }
 
-            MoveCursor(frameTimeInterval);
+            //if(targetPosition != new Vector2(-1f,-1f))
+                MoveCursor(frameTimeInterval);
 
             /*particleAttractionPosition.X += (float)Random.NextDouble(-0.03, 0.03);
             particleAttractionPosition.Y += (float)Random.NextDouble(-0.03, 0.03);*/
+        }
+
+        private void CheckItems(Level level)
+        {
+            int step = level.GameStatistics.Steps-1;
+            switch (ItemSlot)
+            {
+                case Item.ItemType.DANGER_ZONE:
+                    if (targetSpawnPoint != null && (targetPosition - cursorPosition).Length() < 0.02f)
+                    {
+                        UseItem(level);
+                    }
+                    break;
+                case Item.ItemType.MUTATION:
+                    if (level.GameStatistics.getDominationInStep(Index, step) < 0.20f)
+                    {
+                        UseItem(level);
+                    }
+                    break;
+                case Item.ItemType.WIPEOUT:
+                    uint overall = level.GameStatistics.getParticlesInStep(step);
+                    if ((float)NumParticlesAlive / overall < 0.25f)
+                    {
+                        UseItem(level);
+                    }
+                    for(int i=0;i<level.GameStatistics.PlayerCount;++i)
+                    {
+                        if(i!= Index)
+                            if (level.GameStatistics.getPossessingSpawnPointsInStep(i, step) == 0)
+                            {
+                                UseItem(level);
+                            }
+                    }
+                    break;
+                default: break;
+            }
+        }
+
+        private void UseItem(Level level)
+        {
+            level.PlayerUseItem(this);
+            ItemSlot = Item.ItemType.NONE;
+        }
+
+        private void CheckTargets()
+        {
+            if (targetSpawnPoint != null)
+            {
+                if (targetSpawnPoint.PossessingPlayer == Index)
+                {
+                    targetSpawnPoint = null;
+                    currentTargetTime = maxTargetTime + 0.1f;
+                }
+            }
         }
 
         private void SelectTarget(Level level)
@@ -45,6 +117,9 @@ namespace ParticleStormControl
 
             // find nearest not owning spawnpoint;
             var otherSpawnPoints = level.SpawnPoints.Where(x => x.PossessingPlayer != Index);
+            // remove current target to prevent that to much time is spent on an unreachable target
+            if (otherSpawnPoints.Count() > 1 && targetSpawnPoint != null)
+                otherSpawnPoints = otherSpawnPoints.Where(x => x != targetSpawnPoint);
             // find nearest spawn point which no player tries to capture
             var noOtherPlayerTriesToCapture = otherSpawnPoints.Where(x => (x.CapturingPlayer == Index || x.CapturingPlayer == -1));
 
@@ -58,6 +133,7 @@ namespace ParticleStormControl
                     {
                         minDistSq = newDistSq;
                         targetPosition = spawn.Position;
+                        targetSpawnPoint = spawn;
                     }
                 }
             }
@@ -70,6 +146,7 @@ namespace ParticleStormControl
                     {
                         minDistSq = newDistSq;
                         targetPosition = spawn.Position;
+                        targetSpawnPoint = spawn;
                     }
                 }
             }
@@ -81,6 +158,8 @@ namespace ParticleStormControl
         private void MoveCursor(float frameTimeInterval)
         {
             Vector2 cursorMove = targetPosition - cursorPosition;
+            if (cursorMove.Length() < 0.02f) return;
+
             cursorMove /= cursorMove.Length();
             cursorMove *= frameTimeInterval * CURSOR_SPEED;
 
--- ParticleStormControl/ParticleStormControl/InGame/Player/Player.cs
+++ ParticleStormControl/ParticleStormControl/InGame/Player/Player.cs
@@ -85,9 +85,9 @@ namespace ParticleStormControl
                 //new Vector2(Level.RELATIVE_MAX.X/2, Level.RELATIVE_MAX.Y/2)
             };
 #else
-        private readonly static float cursor_offset_x = 0.2f;
-        private readonly static float cursor_offset_y = 0.04f;
-        private readonly static Vector2[] cursorStartPositions =
+        protected readonly static float cursor_offset_x = 0.2f;
+        protected readonly static float cursor_offset_y = 0.04f;
+        protected readonly static Vector2[] cursorStartPositions =
             {
                 new Vector2(0.2f + cursor_offset_x, Level.RELATIVE_MAX.Y-0.2f - cursor_offset_y),
                 new Vector2(Level.RELATIVE_MAX.X-0.2f - cursor_offset_x, 0.2f + cursor_offset_y),
