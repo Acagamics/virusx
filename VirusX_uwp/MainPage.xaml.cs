34efdf51f4de31eb6a5cd6f613a42713ac25ee18
Andreas Reich <r_andreas2@web.de>
2016-07-07 23:07:59 +0100

    Prepared project structure for multi platform. Shared code is now in a shared project.

--- VirusX_uwp/MainPage.xaml.cs
+++ VirusX_uwp/MainPage.xaml.cs
@@ -21,17 +21,17 @@ namespace VirusX
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-public sealed partial class GamePage : Page
-{
-    readonly VirusX _game;
-
-    public GamePage()
+    public sealed partial class GamePage : Page
     {
-        this.InitializeComponent();
+        readonly VirusX _game;
 
-        // Create the game.
-        var launchArguments = string.Empty;
-        _game = MonoGame.Framework.XamlGame<VirusX>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        public GamePage()
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+            var launchArguments = string.Empty;
+            _game = MonoGame.Framework.XamlGame<VirusX>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
     }
-}
 }
